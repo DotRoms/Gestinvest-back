d8bf704a72fb9284f93ce8534cd4deaca84cd839
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-03-29 14:24:33 +0100

    test: server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import "dotenv/config";
 import express from "express";
 
+const PORT = process.env.PORT || 8080;
+
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(PORT, () => {
+  console.log(`server listen on http://localhost:${PORT}`);
+});
