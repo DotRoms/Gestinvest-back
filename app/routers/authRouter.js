dacf3796a344c437c1704d12b497e46cd3c5524f
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-04-04 09:17:33 +0200

    feat : adding test rout for deployement

--- app/routers/authRouter.js
+++ app/routers/authRouter.js
@@ -6,5 +6,6 @@ const router = express.Router();
 // routes auth
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.get('/', (req, res) => res.send('Hello World!'));
 
 export default router;
