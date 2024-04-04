b24635be5c25e1767dfbbf5f98d0d8fa95c16741
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-04-04 09:20:26 +0200

    feat : adding test router for deployement

--- app/routers/index.api.router.js
+++ app/routers/index.api.router.js
@@ -2,7 +2,7 @@ import express from 'express';
 import authRouter from './authRouter.js';
 
 const router = express.Router();
-
+router.get('/', (req, res) => res.send('Hello World!'));
 router.use('/api/authRouter', authRouter);
 
 export default router;
