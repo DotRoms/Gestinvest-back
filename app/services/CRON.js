ec1773ec41ff62f3c646a1dc65743de2378d4117
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-04-09 10:08:25 +0200

    test: test cron

--- app/services/CRON.js
+++ app/services/CRON.js
@@ -1,17 +1,21 @@
-import cron from 'node-cron';
 import 'dotenv/config';
-import stockApi from './API/api.stock.js';
+import cron from 'node-cron';
 import cryptoApi from './API/api.crypto.js';
+import stockApi from './API/api.stock.js';
 
 // Définir la tâche cron
-const task = cron.schedule('30 10 * * *', async () => {
-  await cryptoApi.getPriceCrypto(1, 60);
-  await stockApi.getPriceStock(2, 40);
-  // Code à exécuter chaque minute
-}, {
-  scheduled: true,
-  timezone: 'Europe/Paris' // Spécifiez votre fuseau horaire si nécessaire
-});
+const task = cron.schedule(
+  '10 10 * * *',
+  async () => {
+    await cryptoApi.getPriceCrypto(1, 60);
+    await stockApi.getPriceStock(2, 40);
+    // Code à exécuter chaque minute
+  },
+  {
+    scheduled: true,
+    timezone: 'Europe/Paris' // Spécifiez votre fuseau horaire si nécessaire
+  }
+);
 
 // Lancer la tâche cron
 export default task;
