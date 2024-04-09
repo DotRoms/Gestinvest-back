176d0960cc4c75ca0785802b21f2a3ccb3565eb3
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-04-09 11:06:05 +0200

    test: heroku deploy

--- app/services/API/api.stock.js
+++ app/services/API/api.stock.js
@@ -1,11 +1,130 @@
+// import axios from 'axios';
+// import 'dotenv/config';
+// import assetDatamapper from '../../datamappers/asset.datamapper.js';
+// import groupSymbols from '../../utils/update.prices.api.js';
+
+// export default {
+//   // On récupère les prix des actifs grace à l'api Yahoo Finance
+//   async  fetchQuotesForGroup(group) {
+//     console.log(group);
+//     const options = {
+//       method: 'GET',
+//       url: 'https://yh-finance.p.rapidapi.com/market/v2/get-quotes',
+//       params: {
+//         region: 'US',
+//         symbols: group
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': '1b92d523d0msh23c22511d5db90ep1997f0jsnc943a54e2ef7',
+//         'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com'
+//       }
+//     };
+
+//     try {
+//       const response = await axios.request(options);
+//       return response.data;
+//     } catch (error) {
+//       console.error(error);
+//       return null;
+//     }
+//   },
+
+//   // On récupère les prix des actifs et on les met à jour en BDD
+//   async  getPriceStock(categoryId, groupSize) {
+//     const groups = await groupSymbols.getSymbolsInGroups(categoryId, groupSize);
+
+//     try {
+//       const responses = await Promise.all(groups.map(this.fetchQuotesForGroup));
+//       const result = [];
+//       // Pour accéder aux données dans chaque réponse
+//       responses.forEach((response) => {
+//         for (let i = 0; i < response.quoteResponse.result.length; i += 1) {
+//           result.push({
+//             [`${response.quoteResponse.result[i].symbol}`]:
+//             response.quoteResponse.result[i].regularMarketPrice
+//           });
+//         }
+//       });
+
+//       // On itère sur le résultat pour récupérer les prix et les mettre à jour en BDD
+//       result.forEach((item) => {
+//         Object.keys(item).forEach(async (key) => {
+//           await assetDatamapper.updatePrices(key, item[key]);
+//         });
+//       });
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// };
+// // await getPrice(2, 40);
+
 import axios from 'axios';
 import 'dotenv/config';
 import assetDatamapper from '../../datamappers/asset.datamapper.js';
 import groupSymbols from '../../utils/update.prices.api.js';
 
+const RATE_LIMIT_DELAY = 1000; // Délai en millisecondes entre chaque requête (1000 ms = 1 seconde)
+let lastRequestTimestamp = 0; // Timestamp de la dernière requête
+
+async function delayIfNeeded() {
+  // Obtient le timestamp actuel
+  const now = Date.now();
+  // Calcule le temps écoulé depuis la dernière requête
+  const timeSinceLastRequest = now - lastRequestTimestamp;
+
+  // Vérifie si le temps écoulé est inférieur au délai limite autorisé entre les requêtes
+  if (timeSinceLastRequest < RATE_LIMIT_DELAY) {
+    // Calcule le délai restant avant de pouvoir effectuer la prochaine requête
+    const delay = RATE_LIMIT_DELAY - timeSinceLastRequest;
+    // Attend le délai restant avant de résoudre la promesse, ce qui suspend l'exécution
+    await new Promise((resolve) => {
+      setTimeout(resolve, delay);
+    });
+  }
+
+  // Met à jour le timestamp de la dernière requête pour le prochain contrôle
+  lastRequestTimestamp = Date.now();
+}
+
 export default {
+  // On récupère les prix des actifs et on les met à jour en BDD
+  async getPriceStock(categoryId, groupSize) {
+    const groups = await groupSymbols.getSymbolsInGroups(categoryId, groupSize);
+
+    try {
+      const responses = await Promise.all(
+        groups.map(async (group) => {
+          await delayIfNeeded(); // Attendre avant chaque requête
+          return this.fetchQuotesForGroup(group);
+        })
+      );
+
+      const result = [];
+      // Pour accéder aux données dans chaque réponse
+      responses.forEach((response) => {
+        for (let i = 0; i < response.quoteResponse.result.length; i += 1) {
+          result.push({
+            [`${response.quoteResponse.result[i].symbol}`]:
+              response.quoteResponse.result[i].regularMarketPrice
+          });
+        }
+      });
+
+      // On itère sur le résultat pour récupérer les prix et les mettre à jour en BDD
+      result.forEach(async (item) => {
+        Object.keys(item).forEach(async (key) => {
+          await assetDatamapper.updatePrices(key, item[key]);
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
   // On récupère les prix des actifs grace à l'api Yahoo Finance
-  async  fetchQuotesForGroup(group) {
+  async fetchQuotesForGroup(group) {
+    console.log(group);
     const options = {
       method: 'GET',
       url: 'https://yh-finance.p.rapidapi.com/market/v2/get-quotes',
@@ -26,34 +145,5 @@ export default {
       console.error(error);
       return null;
     }
-  },
-
-  // On récupère les prix des actifs et on les met à jour en BDD
-  async  getPriceStock(categoryId, groupSize) {
-    const groups = await groupSymbols.getSymbolsInGroups(categoryId, groupSize);
-
-    try {
-      const responses = await Promise.all(groups.map(this.fetchQuotesForGroup));
-      const result = [];
-      // Pour accéder aux données dans chaque réponse
-      responses.forEach((response) => {
-        for (let i = 0; i < response.quoteResponse.result.length; i += 1) {
-          result.push({
-            [`${response.quoteResponse.result[i].symbol}`]:
-            response.quoteResponse.result[i].regularMarketPrice
-          });
-        }
-      });
-
-      // On itère sur le résultat pour récupérer les prix et les mettre à jour en BDD
-      result.forEach((item) => {
-        Object.keys(item).forEach(async (key) => {
-          await assetDatamapper.updatePrices(key, item[key]);
-        });
-      });
-    } catch (error) {
-      console.error(error);
-    }
   }
 };
-// await getPrice(2, 40);
--- app/services/CRON.js
+++ app/services/CRON.js
@@ -3,16 +3,36 @@ import cron from 'node-cron';
 import cryptoApi from './API/api.crypto.js';
 import stockApi from './API/api.stock.js';
 
-// Définir la tâche cron
-const task = cron.schedule(
-  '35 10 * * *',
-  async () => {
-    console.log('running task');
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function executeTaskWithRetry() {
+  try {
     await cryptoApi.getPriceCrypto(1, 60);
     console.log('midle task');
-    await stockApi.getPriceStock(2, 10);
+    await stockApi.getPriceStock(2, 40);
     console.log('done');
-    // Code à exécuter chaque minute
+  } catch (error) {
+    if (error.response && error.response.status === 429) {
+      // Si l'erreur est 429, réessayez après un certain délai
+      console.log('Too many requests. Retrying after delay...');
+      await wait(60000); // Attendre 1 minute avant de réessayer (vous pouvez ajuster cette valeur selon les recommandations de l'API)
+      await executeTaskWithRetry(); // Réessayer la tâche
+    } else {
+      // Gérer d'autres erreurs
+      console.error('Error:', error.message);
+    }
+  }
+}
+
+const task = cron.schedule(
+  '57 10 * * *',
+  async () => {
+    console.log('running task');
+    await executeTaskWithRetry();
   },
   {
     scheduled: true,
@@ -20,5 +40,4 @@ const task = cron.schedule(
   }
 );
 
-// Lancer la tâche cron
 export default task;
