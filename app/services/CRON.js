29090f38a07812a07485e9e68dc27d10b623bb93
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-04-09 10:21:39 +0200

    test: heroku deploy

--- app/services/CRON.js
+++ app/services/CRON.js
@@ -5,7 +5,7 @@ import stockApi from './API/api.stock.js';
 
 // Définir la tâche cron
 const task = cron.schedule(
-  '10 10 * * *',
+  '25 10 * * *',
   async () => {
     await cryptoApi.getPriceCrypto(1, 60);
     await stockApi.getPriceStock(2, 40);
