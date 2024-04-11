8e0b2b92ed4d1d6433630aadc0b32cb60b4863e0
Hugo-Marecal <hugo.marecal@oclock.school>
2024-04-11 14:25:42 +0200

    feat: change settings for addLine function

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -6,26 +6,46 @@ import tradingOperationDatamapper from '../datamappers/tradingOperation.datamapp
 
 const dashboard = {
   async dashboardDetail(req, res) {
+    // On récupère l'id de l'utilisateur
     const { id } = req.user;
+
+    // On récupère toutes les lignes d'investissement de l'utilisateur et on fait tout les calculs
     const allLines = await dashboardDatamapper.findAllTradingLinesByUser(id);
     const assetInformationByUser = calculateAssetInformation.getAssetUserInformation(allLines);
     res.json({ userInformation: assetInformationByUser });
   },
 
   async openModal(req, res) {
+    // On récupère tout les assets avec leurs catégories
     const allAsset = await assetDatamapper.findAssetNameAndCategory();
     res.json({ allAsset });
   },
 
   async addLine(req, res) {
-    const { data } = req.body;
+    // On récupère l'id de l'utilisateur
     const { id } = req.user;
+
+    // On récupère l'url pour savoir si c'est un achat ou une vente
+    const { url } = req;
+    const tradingOperationType = url.substring(1, url.indexOf('?'));
+
+    // On récupère les données de la requête (modal achat/vente)
+    const { data } = req.body;
+
+    // On récupère l'id du portfolio de l'utilisateur
     const portfolioId = await userDatamapper.getPortfolioByUserId(id);
-    const tradingType = await tradingOperationDatamapper.getOperationByName(data.tradingOperationType);
+
+    // On récupère l'id du type de l'opération
+    const tradingTypeId = await tradingOperationDatamapper.getOperationByName(tradingOperationType);
+
+    // On récupère l'id de l'asset que l'on achète ou vend
     const assetId = await assetDatamapper.getAssetId(data.assetName);
+
+    // On récupère toutes les lignes d'investissement de l'utilisateur et on fait tout les calculs
     const allLines = await dashboardDatamapper.findAllTradingLinesByUser(id);
     const assetInformationByUser = calculateAssetInformation.getAssetUserInformation(allLines);
 
+    // On vérifie si l'utilisateur ne vend pas plus d'asset qu'il n'en possède
     if (data.tradingOperationType === 'sell') {
       const invalidData = assetInformationByUser.assetUserInformation.find((obj) => obj.assetName.toLowerCase() === data.assetName.toLowerCase() && obj.quantity < data.assetNumber);
 
@@ -34,6 +54,7 @@ const dashboard = {
       }
     }
 
+    // On regroupe les data pour pouvoir les ajouter en base de données
     const newData = {
       assetId: assetId.id,
       portfolioId: portfolioId.id,
@@ -41,9 +62,12 @@ const dashboard = {
       price: data.price,
       fees: data.fees,
       date: data.date,
-      tradingOperationTypeId: tradingType.id
+      tradingOperationTypeId: tradingTypeId.id
     };
+
+    // Une fois les vérifications effectuées, on ajoute la ligne en base de données
     await dashboardDatamapper.addLine(newData);
+
     return res.json({ successMessage: 'Ajout bien effectuer' });
   }
 };
