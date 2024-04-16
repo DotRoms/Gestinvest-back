3f308713af659e97953a623abd918d5725096406
Hugo-Marecal <hugo.marecal@oclock.school>
2024-04-16 10:07:58 +0200

    feat: add getUser function

--- app/controllers/accountController.js
+++ app/controllers/accountController.js
@@ -1,6 +1,41 @@
+import userDatamapper from '../datamappers/user.datamapper.js';
+
 const accountController = {
   async getUSer(req, res) {
+    // On récupère l'id de l'utilisateur connecté
     const { id } = req.user;
+
+    // On récupère les informations de l'utilisateur depuis la BDD
+    const Oneuser = await userDatamapper.findOne(id);
+
+    // On supprime le mot de passe de l'utilisateur
+    res.json({ user: Oneuser });
+  },
+
+  async updateUser(req, res) {
+    // On récupère les infos du formulaire de mise à jour
+    const newEmail = req.body.email;
+    const { firstname } = req.body;
+
+    // On récupère l'id de l'utilisateur connecté
+    const { id } = req.user;
+
+    // On récupère les informations de l'utilisateur depuis la BDD
+    const Oneuser = await userDatamapper.findOne(id);
+
+    // On vérifie que l'email est valide avec une regex
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailRegex.test(email)) {
+      res.status(400).json({ errorMessage: 'Email invalide' });
+      return;
+    }
+
+    // On vérifie que l'email n'est pas déjà utilisé
+    const alreadyExistingUser = await userDatamapper.findByEmail(email);
+    if (alreadyExistingUser) {
+      res.status(400).json({ errorMessage: 'Cet email est déjà utilisé' });
+    }
   }
 };
 
--- app/routers/accountRouter.js
+++ app/routers/accountRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import controllerWrapper from '../middlewares/controller.wrapper.middleware.js';
 import accountController from '../controllers/accountController.js';
 
 const router = express.Router();
 
-router.get('/user', accountController.getUSer);
+router.get('/', controllerWrapper(accountController.getUSer));
+router.patch('/update', controllerWrapper(accountController.updateUser));
 
 export default router;
